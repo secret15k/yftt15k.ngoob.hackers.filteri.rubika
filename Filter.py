2be8d50275d6f8d85bbbfea06e9cd5576fb6b610
secret15k <115003649+secret15k@users.noreply.github.com>
2022-10-09 00:21:06 +0330

    Create Filter.py

---
+++ Filter.py
@@ -0,0 +1,7 @@
+print ("http://yttk15.12.22.Insulting.the.leader.and.obscenity.rest")
+
+
+print ("http://6.0.3.6/d///f//i///y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4)((8.0.8.3/kgj/af0////4.6.7.8.2.3.5.6.7.3.2.9.3.9.1")
+
+
+#Dx prit
