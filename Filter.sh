dbf702a4e2fab845f1eb78984fbac12cac39d472
secret15k <115003649+secret15k@users.noreply.github.com>
2022-10-09 19:44:20 +0330

    Create Filter.sh

---
+++ Filter.sh
@@ -0,0 +1,32 @@
+# yftt15k.ngoob.hackers.filteri.rubika
+
+
+print ("pornhub.com")
+
+
+print ("xnxx.com")
+
+
+print ("http://Dxprit.yftt15k.ngoob.hackers.filteri.rubika")
+
+print ("rubika.com")
+
+
+print ("Filtering.rubika.com.1.2.3.xxx.spam/")
+
+
+
+print ("http://mirror.mephi.ru/termux/termux-main")
+
+
+
+print ("http://mirror.mephi.ru/termux/termux-main")
+
+
+print ("http://mirror.mephi.ru/termux/termux-main")
+
+
+print ("Oct 09 19:40:14.347 [notice] Tor 0.4.7.10 running on Linux with Libevent 2.1.12-stable, OpenSSL 3.0.5, Zlib 1.2.12, Liblzma 5.2.7, Libzstd N/A and Unknown N/A as libc.")
+
+
+print ("https://support.torproject.org/faq/staying-anonymous/")
